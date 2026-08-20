f43948dc8e95ba2624afa5279e68d920e5f98733
shreeshail <shreeshailbiradar833@gmail.com>
2026-08-21 00:14:33 +0530

    Fix user ID generation

--- src/main/java/com/Shree/ecom_web/model/Users.java
+++ src/main/java/com/Shree/ecom_web/model/Users.java
@@ -1,12 +1,25 @@
 package com.Shree.ecom_web.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String role;
+
     public int getId() {
         return id;
     }
@@ -23,20 +36,20 @@ public class Users {
         this.username = username;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -70,25 +83,4 @@ public class Users {
     public void setRole(String role) {
         this.role = role;
     }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(unique = true, nullable = false)
-    private String username;
-
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String phone;
-
-    private String role = "USER";
 }
