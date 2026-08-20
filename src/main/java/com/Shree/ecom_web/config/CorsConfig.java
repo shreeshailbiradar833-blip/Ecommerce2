a1c2d0abe174bec92ce17666ad9e8f6cbbbd20bb
shreeshail <shreeshailbiradar833@gmail.com>
2026-08-21 02:22:03 +0530

    Fix CORS for Netlify

--- src/main/java/com/Shree/ecom_web/config/CorsConfig.java
+++ src/main/java/com/Shree/ecom_web/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:5173",
-                        "https://YOUR-NETLIFY-SITE.netlify.app"
+                        "https://novocart.netlify.app"
                 )
         );
 
